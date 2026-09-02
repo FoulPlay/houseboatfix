f6e06f56da436a91edb508654333732356c7e838
FoulPlay <FoulPlay@users.noreply.github.com>
2026-09-02 12:09:36 +0000

    Upload 1.0.0.1a Source (Part 3)

---
+++ 1.0.0.1a-src/houseboatfix/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using Sims3.SimIFace;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("houseboatfix")]
+[assembly: AssemblyProduct("houseboatfix")]
+[assembly: AssemblyCopyright("Copyright 2026")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("foulplay phantomsimmer thesammy58 unjust_harry fleshtexture lazyduchess anime_boom destrospean Simler90")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: Tunable]
+[assembly: PersistableStatic]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
